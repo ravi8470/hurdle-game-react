b14a70c7a6f6427abe7c00878d85185901d75115
Ravi Kumar <rvkmr0851@gmail.com>
2020-09-24 21:24:45 +0530

    setup db connection

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var mongoose = require("mongoose");
 var cors = require('cors');
 
+const InitiateMongoServer = require("./config/db.config");
+InitiateMongoServer();
+
 var app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true })); 
@@ -11,6 +14,7 @@ app.get('/', (req, res) => {
   res.json('Hello from demo app!!!') 
 });
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log('Listening on port ', process.env.PORT || 5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log('Listening on port ', PORT);
 })
